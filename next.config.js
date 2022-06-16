f513bfcd56bb55dd0fd2ae52554c3b121455a0a8
user <pixel22yuliya@gmail.com>
2022-06-16 16:02:44 +0300

    fix cors

--- next.config.js
+++ next.config.js
@@ -10,8 +10,8 @@ const nextConfig = {
   // },
   reactStrictMode: true,
   env: {
-    API_URL: "http://localhost:3000/api/",
-    //API_URL: "https://nextapp-pink-one.vercel.app/api/",
+    // API_URL: "http://localhost:3000/api/",
+    API_URL: "https://nextapp-pink-one.vercel.app/api/",
   },
 };
 
