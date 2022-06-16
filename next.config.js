6aed71274bf81ca77eb8446fb37ccb5c46f62ec7
user <pixel22yuliya@gmail.com>
2022-06-16 12:40:21 +0300

    fix cors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://api.example.com/:path*",
+      },
+    ];
+  },
   reactStrictMode: true,
   env: {
     API_URL: "http://localhost:3000/api/",
--- pages/api/goods/index.ts
+++ pages/api/goods/index.ts
@@ -1,4 +1,4 @@
-import { goods } from "./data/goods";
+import { goods } from "../data/goods";
 export default function Goods(req: any, res: any) {
   res.status(200).json(goods);
 }
