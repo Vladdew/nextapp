a613e86d34f298b5656b5ef7c1d0578b3d240afe
user <pixel22yuliya@gmail.com>
2022-06-16 13:35:07 +0300

    fix cors

--- pages/api/good/[id].ts
+++ pages/api/good/[id].ts
@@ -1,5 +1,6 @@
 import { goods } from "../data/goods";
 
 export default function getGoodById(req: any, res: any) {
+  console.log(goods[req.query.id]);
   res.status(200).json(goods[req.query.id]);
 }
