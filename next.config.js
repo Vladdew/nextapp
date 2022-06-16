902ed6598c77860adad436c60c3d73cc840a5e61
user <pixel22yuliya@gmail.com>
2022-06-16 12:47:35 +0300

    fix cors

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "https://api.example.com/:path*",
+        destination: "https://nextapp-pink-one.vercel.app/:path*",
       },
     ];
   },
