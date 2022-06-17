117c72cae8a68ca7e06a27c4ac52e0cbb447cff1
user <pixel22yuliya@gmail.com>
2022-06-17 16:25:02 +0300

    add weather

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://nextapp-pink-one.vercel.app/:path*",
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       destination: "https://magenta-malabi-98aec4.netlify.app/:path*",
+  //     },
+  //   ];
+  // },
   reactStrictMode: true,
   env: {
-    API_URL: "http://localhost:3000/api",
+    API_URL: "https://magenta-malabi-98aec4.netlify.app/api",
   },
 };
 
